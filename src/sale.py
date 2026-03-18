25c354bc77be9739f0560960e206fb05bc90a4d3
Iván Pérez <ivan52p@gmail.com>
2026-03-18 18:41:02 +0100

    Update sale.py

--- src/sale.py
+++ src/sale.py
@@ -1,11 +1,11 @@
 class Sale:
     def __init__(self, sale_id, client_id, product, category, amount, date):
-        self.sale_id = sale_id
-        self.client_id = client_id
-        self.product = product
-        self.category = category
-        self.amount = amount
-        self.date = date
+        self.sale_id = int(sale_id)
+        self.client_id = int(client_id)
+        self.product = str(product)
+        self.category = str(category)
+        self.amount = float(amount)
+        self.date = str(date)
 
     def to_dict(self):
         return {
