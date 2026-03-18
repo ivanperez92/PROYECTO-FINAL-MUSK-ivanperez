089b7bc06ce4a08b11f4b93049453003f83fe161
Iván Pérez <ivan52p@gmail.com>
2026-03-18 18:40:22 +0100

    Update client.py

--- src/client.py
+++ src/client.py
@@ -1,9 +1,9 @@
 class Client:
     def __init__(self, client_id, name, country, signup_date):
-        self.client_id = client_id
-        self.name = name
-        self.country = country
-        self.signup_date = signup_date
+        self.client_id = int(client_id)
+        self.name = str(name)
+        self.country = str(country)
+        self.signup_date = str(signup_date)
 
     # convertir el objeto a un diccionario y exportarlo como JSON.
     def to_dict(self):
