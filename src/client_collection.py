bb97d2301fd6ff8641860b7e96e15c447389ddae
Iván Pérez <ivan52p@gmail.com>
2026-03-18 19:14:21 +0100

    Update client_collection.py

--- src/client_collection.py
+++ src/client_collection.py
@@ -4,7 +4,7 @@ from client import Client
 
 class ClientCollection:
     def __init__(self):
-        self.clients = []
+        self.clients = [] # Lista vacía para almacenar los objetos Client.
 
     def importar_clientes_JSON(self, file_path):
         with open(file_path, 'r', encoding='utf-8') as file:
